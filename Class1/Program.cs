c3816e9742ec1ee38b26bafb3a873506cd153ba6
jenifery <jenifers@revature.com>
2023-02-07 04:40:10 -0500

    first

---
+++ Class1/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using Class1;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Singleton obj1 = Singleton.GetInstances;
+        obj1.PrintDetails("Trainer");
+        Singleton obj2 = Singleton.GetInstances;
+        obj2.PrintDetails("Student");
+
+
+        //initialize null
+
+        /* int? a = 20;
+         int b = 10;
+         int c = a ?? b;
+         Console.WriteLine(c);*/
+
+        // Console.WriteLine("hello world"); - TC :O(1) SC:O(1)
+        /*  int n = 10;
+         for (int i = 1; i<=n;i++)
+          {
+              Console.WriteLine("hello world");
+          }
+          // O(n)*/
+
+      /*  int n = 8;
+        for (int i =1;i<=n;i=i*2)
+        {
+            Console.WriteLine("hello world");
+        }
+
+        //O(log2(n))*/
+
+
+    }
+}
---
+++ Class1/Singleton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class1
+{
+    internal class Singleton
+    {
+        private static int counter = 0;
+        private static Singleton instance = null;
+
+        public static Singleton GetInstances
+        {
+            get
+            {
+                if(instance==null)
+                {
+                    instance = new Singleton();
+                    
+                }
+                return instance;
+            }
+        }
+
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("Counter value :" + counter);
+        }
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
